a71f1507e7abb2566133614e7c8a0a660feb8ca5
vsaldivarm <121958891+vsaldivarmreece@users.noreply.github.com>
2023-07-26 12:43:04 -0600

    configurate basic di

--- app/src/main/java/com/vsaldivarm/readerapp/MainActivity.kt
+++ app/src/main/java/com/vsaldivarm/readerapp/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
 import com.vsaldivarm.readerapp.ui.theme.ReaderAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 import java.util.HashMap
 
+@AndroidEntryPoint //it will receive all DI
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
