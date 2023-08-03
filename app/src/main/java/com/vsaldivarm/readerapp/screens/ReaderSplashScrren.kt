6c47966e1e0ac279b43450626d48948030c18438
vsaldivarm <121958891+vsaldivarmreece@users.noreply.github.com>
2023-08-03 12:55:55 -0600

    create animation for splash screen

--- app/src/main/java/com/vsaldivarm/readerapp/screens/ReaderSplashScrren.kt
+++ app/src/main/java/com/vsaldivarm/readerapp/screens/ReaderSplashScrren.kt
@@ -1,5 +1,7 @@
 package com.vsaldivarm.readerapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,32 +14,62 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
-fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)){
-   Surface(modifier = Modifier
-      .padding(15.dp)
-      .size(330.dp)
-   , shape = CircleShape,
-   color = Color.White,
-      border = BorderStroke(2.dp,Color.LightGray)
+fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+   val scale = remember {
+      androidx.compose.animation.core.Animatable(0f)
+   }
+   //animation
+   LaunchedEffect(key1 = true) {
+      scale.animateTo(
+         targetValue = 0.9f,
+         animationSpec = tween(durationMillis = 800,
+            easing = {
+               OvershootInterpolator(8f)
+                  .getInterpolation(it)
+            })
+      )
+      delay(2000L)
+   }
+
+   Surface(
+      modifier = Modifier
+         .padding(20.dp)
+         .size(330.dp)
+         .scale(scale.value),
+      shape = CircleShape,
+      color = Color.White,
+      border = BorderStroke(
+         2.dp,
+         Color.LightGray
+      )
    ) {
-      Column(modifier = Modifier.padding(1.dp),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center) {
-         Text(text = "Reader", style = MaterialTheme.typography.displayLarge,
-         color = Color.Red.copy(alpha = 0.5f))
+      Column(
+         modifier = Modifier.padding(1.dp),
+         horizontalAlignment = Alignment.CenterHorizontally,
+         verticalArrangement = Arrangement.Center
+      ) {
+         Text(
+            text = "Reader", style = MaterialTheme.typography.displayLarge,
+            color = Color.Red.copy(alpha = 0.5f)
+         )
          Spacer(modifier = Modifier.height(15.dp))
-         Text(text = "\"Change. Yourself \"",
-            style = MaterialTheme.typography.displaySmall,
+         Text(
+            text = "\"Change. Yourself \"",
+            style = MaterialTheme.typography.titleLarge,
             color = Color.LightGray
          )
       }
